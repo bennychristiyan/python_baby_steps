e1c6e99409655a7a80551a567ec8c222cc0c7580
bennychristiyan <bennychristiyan89@gmail.com>
2023-03-26 19:58:28 +0530

    fixed 2 files from idle format to python format

--- numbers/BODMAS_concept.py
+++ numbers/BODMAS_concept.py
@@ -1,6 +1,17 @@
-Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 2 * 5 - 7
+"""
+this program is to demonstrate the BODMAS concept
+Author: Benny
+"""
+
+# without bracket
+print(2 * 5 - 7)
+
+# with bracket
+print(2 * (9 - 7))
+
+# output
+"""
 3
->>> 2 * (9 - 7)
 4
+
+"""
---
+++ numbers/basic_arithmetic_floats_modulo.py
@@ -0,0 +1,35 @@
+# addition
+print(2 + 3) 
+
+# subtraction
+print(5 - 4)
+
+# division
+print(5 / 4)
+print(6 / 3)
+
+# multiplication
+print(3 * 7)
+
+# modulo division
+print(5 % 6)
+print(7 % 3)
+
+# type of numbers
+print(type(2)) # int
+print(type(0.3)) # float
+
+# output
+"""
+5
+1
+1.25
+2.0
+21
+5
+1
+<class 'int'>
+<class 'float'>
+
+"""
+
